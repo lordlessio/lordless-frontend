16eed2a40095acb164bf2b090eb8656fdab67fc9
foooooouny <794485409@qq.com>
2018-08-15 20:48:57 +0800

    fixed svg

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,7 +4,6 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
-console.log(' base config', config.build.assetsPublicPath)
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
