a34c3c60c08d2c44f1edc422b7f85174b395c8b3
foooooouny <794485409@qq.com>
2018-08-29 18:55:24 +0800

    rm contract/Index

--- src/contract/index.js
+++ src/contract/index.js
@@ -37,3 +37,4 @@ export const TavernNFTs = (web3js) => initContract(process.env.contract.TavernNF
 export const NFTsCrowdsale = (web3js) => initContract(process.env.contract.NFTsCrowdsale, web3js)
 // export const Power = (web3js) => initContract(process.env.contract.Power, web3js)
 // export const Building = (web3js) => initContract(process.env.contract.Building, web3js)
+
